3c3617eafd0dad3ec277e9a0ccef75bd7e3db761
Jason Heller <jasonheller96@gmail.com>
2024-03-14 12:46:37 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion";
